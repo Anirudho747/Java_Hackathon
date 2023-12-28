319ba8e4b025668537cea714b640b1138c11301c
Ani <anirudho747@gmail.com>
2023-12-28 07:00:20 +0530

    Parameter updated

--- src/week2/Day13_returnCorrectSequence.java
+++ src/week2/Day13_returnCorrectSequence.java
@@ -2,8 +2,8 @@ package week2;
 
 public class Day13_returnCorrectSequence {
 
- //   static String str="so8 too9 i6 love4 leetcode5 think7 and2 alice1 bob3";
-    static String str="alice1 bob3 and2";
+    static String str="so8 too9 i6 love4 leetcode5 think7 and2 alice1 bob3";
+  //  static String str="alice1 bob3 and2";
     public static void main(String[] args)
     {
         getSequence();
@@ -23,7 +23,7 @@ public class Day13_returnCorrectSequence {
             res[j-1] = strArray[i].substring(0,strArray[i].length()-1);
         }
 
-        for(int i=0;i<3;i++)
+        for(int i=0;i<res.length;i++)
         {
             System.out.println(res[i]);
         }
