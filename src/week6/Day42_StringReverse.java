0cdd06afec6bf4e2630c497b08a808444a8e00a2
Ani <anirudho747@gmail.com>
2024-03-25 07:21:32 +0530

    Reverse String 2

--- src/week6/Day42_StringReverse.java
+++ src/week6/Day42_StringReverse.java
@@ -12,16 +12,33 @@ public class Day42_StringReverse {
         System.out.println("Enter String");
         str = sc.nextLine();
 
-        revString(str);
+        revStringandWord(str);
+        revWord(str);
     }
 
-    static void revString(String str1)
+    static void revStringandWord(String str1)
     {
-        String[] strArray = str.split("");
+        String[] strArray = str1.split("");
 
         for(int i= strArray.length-1;i>=0;i--)
         {
             System.out.print(strArray[i]+" ");
         }
+        System.out.println();
     }
+
+    static void revWord(String str1)
+    {
+        String[] strArray = str1.split(" ");
+
+        for (int i = 0; i <= strArray.length-1; i++)
+        {
+            for(int j=strArray[i].length()-1;j>=0;j--)
+            {
+               System.out.print(strArray[i].charAt(j));
+            }
+            System.out.print(" ");
+        }
+    }
+
 }
